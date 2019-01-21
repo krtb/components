66003b20883a109d5b211805ee9518bb34defe57
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-21 14:28:17 -0500

    displaying divs correctly, text rendering to page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,22 @@ import ReactDOM from 'react-dom'
 
 const App = () => {
     return (
-        <div>Hi There!</div>
+        <div className="ui container comments">
+            <div className="comment">
+                <a href="/" className="avatar">
+                    <img alt="avatar"/>
+                </a>
+                <div className="content">
+                    <a href="/" className="author">
+                        Sam I Am
+                    </a>
+                    <div className="metdata">
+                        <span className="date">Today at 6:00pm</span>
+                    </div>
+                    <div className="text">Nice blog post!</div>
+                </div>
+            </div>
+        </div>
     );
 };
 
