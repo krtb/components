e24f1a87e087a2daf5bf80fa29a8cd18e1e6e108
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-21 15:34:13 -0500

    created component and displayed component nesting

--- src/index.js
+++ src/index.js
@@ -2,24 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom'
 // can treat api as large object, need to add () to use
 import faker from 'faker';
+import CommentDetail from './CommentDetail'
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()} />
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam I Am
-                    </a>
-                    <div className="metdata">
-                        <span className="date">Today at 6:00pm</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
+            <CommentDetail/>
         </div>
     );
 };
