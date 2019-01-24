cf6df8bf3fa5dc3af1621605850323778f5396ce
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-24 14:20:41 -0500

    passing author props from index.js to CommentDetail component

--- src/CommentDetail.js
+++ src/CommentDetail.js
@@ -2,7 +2,6 @@ import React from 'react';
 import faker from 'faker';
 
 const CommentDetail = (props) => {
-    console.log(props)
     return (
         <div className="comment">
             <a href="/" className="avatar">
@@ -10,8 +9,8 @@ const CommentDetail = (props) => {
             </a>
             <div className="content">
                 <a href="/" className="author">
-                    Sam I Am
-                    </a>
+                    {props.author}
+                </a>
                 <div className="metdata">
                     <span className="date">Today at 6:00pm</span>
                 </div>
