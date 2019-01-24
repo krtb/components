9f1248ae127204ff54cf28cba3b34948314a3a14
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-24 14:45:57 -0500

    delete faker import where not needed

--- src/index.js
+++ src/index.js
@@ -6,12 +6,30 @@ import CommentDetail from './CommentDetail'
 
 const App = () => {
     return <div className="ui container comments">
-        <CommentDetail author="Sam" timeAgo="Today at 4:55AM" commentSnip="everything is okay" fakerImg={faker.image.avatar()} />
-        <CommentDetail author="Gwen" timeAgo="TODAY AT 5:00PM" commentSnip="everything is okay" fakerImg={faker.image.avatar()} />
-        <CommentDetail author="Rick" timeAgo="TODAY AT 3:22AM" commentSnip="everything is okay" fakerImg={faker.image.avatar()} />
-        <CommentDetail author="Ricardo" timeAgo="TODAY AT 8:00PM" commentSnip="everything is okay" fakerImg={faker.image.avatar()} />
-        <CommentDetail author="Steve" timeAgo="TODAY AT 10:30AM" commentSnip="everything is okay" fakerImg={faker.image.avatar()} />
-        <CommentDetail author="Babo" timeAgo="TODAY AT 6:00PM" commentSnip="everything is okay" fakerImg={faker.image.avatar()} />
+        <CommentDetail 
+            author="Sam" 
+            timeAgo="Today at 4:55AM" 
+            commentSnip="everything is okay" 
+            fakerImg={faker.image.avatar()} 
+        />
+        <CommentDetail 
+            author="Gwen" 
+            timeAgo="TODAY AT 5:00PM" 
+            commentSnip="everything is okay" 
+            fakerImg={faker.image.avatar()} 
+        />
+        <CommentDetail
+            author="Sam"
+            timeAgo="Today at 4:55AM"
+            commentSnip="everything is okay"
+            fakerImg={faker.image.avatar()}
+        />
+        <CommentDetail
+            author="Gwen"
+            timeAgo="TODAY AT 5:00PM"
+            commentSnip="everything is okay"
+            fakerImg={faker.image.avatar()}
+        />
       </div>;
 };
 
