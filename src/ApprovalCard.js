46973fc632447072626c80fdb555e5401130f582
Kurt Bauer <kurtbauer55@protonmail.com>
2019-01-29 14:23:02 -0500

    start boilerplate code for new, reusable component

---
+++ src/ApprovalCard.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ApprovalCard = () => {
+    return(
+        <div className="ui-card">
+            <div className="content">
+
+            </div>
+        </div>
+    )
+}
+
+export default ApprovalCard; 
